6a0bb01d946137036e9ddaa2dc0b388e96d99dab
bgarbitt <bgarbitt@ualberta.ca>
2018-06-04 15:47:54 -0600

    Finished coding part of tutorial

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -7,3 +7,108 @@ from flaskr.auth import login_required
 from flaskr.db import get_db
 
 bp = Blueprint('blog', __name__)
+
+@bp.route('/')
+def index():
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('blog/index.html', posts=posts)
+
+
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO post (title, body, author_id)'
+                ' VALUES (?, ?, ?)',
+                (title, body, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+    return render_template('blog/create.html')
+
+def get_post(id, check_author=True):
+    post = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    if post is None:
+        abort(404, "Post id {0} doesn't exist.".format(id))
+
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
+'''
+abort() will raise a special exception that returns a HTTP status code. It takes
+an optional message to show with the error, otherwise a default message is used.
+404 means 'not found', and 403 means 'forbidden'. Note: 401 is used for
+'unauthorized', but we detect that earlier in the login page.
+The 'check_author' arguemnt is defined such that the function can be used to get
+a post without checking the author. This would be useful if you write a view to
+show an individual post on a page, where the user doesn't matter because they
+have no way of modifying the post.
+'''
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE post SET title = ?, body = ?'
+                ' WHERE id = ?',
+                (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/update.html', post=post)
+'''
+This view takes an arguement, id. A real URL would look like /1/update. Flask
+will capture the 1, ensure it's an int, and pass it as the id arguement. If you
+don't specify int, and instead do <id>, it will be a string. To generate a URL
+to the update page, url_for() needs to be passed the id so it knows what to fill
+in: url_for('blog.update', id=post['id']). This is also in the index.html file.
+It's possible to combine both the update and insert view.
+'''
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
